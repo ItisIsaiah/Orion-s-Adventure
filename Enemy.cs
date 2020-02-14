4e389587b634a8de098e746c12b705de48e42dff
ZayX731 <55722377+ZayX731@users.noreply.github.com>
2020-02-13 22:49:49 -0500

    Add files via upload

---
+++ Enemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int maxHealth = 100;
+    double currentHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+   public void TakeDamage(double damage)
+    {
+        currentHealth -= damage;
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+    void Die()
+    {
+        Debug.Log("Died");
+    }
+}
---
+++ Melee.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Melee : MonoBehaviour
+{
+   
+    public int ammo=0;
+    public double damage = 0;
+    public Animator ani;
+    public float attackRange = 0.5f;
+    public LayerMask enemies;
+    public Transform attackPoint;
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void attack()
+    {
+        ani.SetTrigger("Attack");
+
+        Collider2D[] hitenemy = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemies);
+
+        foreach (Collider2D enemy in hitenemy)
+        {
+            enemy.GetComponent<Enemy>().TakeDamage(damage);
+        }
+
+        void OnDrawGizmosSelected()
+        {
+            if (attackPoint = null)
+            {
+                return;
+            }
+            Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+        }
+    }
+}
